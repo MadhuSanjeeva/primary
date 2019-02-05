518645ea107ae408617f183e0511899688ba59c0
MadhuSanjeeva <madhusanjeeva91@gmail.com>
2019-02-05 17:25:45 +0530

    removed jars add new spring jars 5.0.2
    Up to Spring Configuration with java code(no xml)

---
+++ spring-demo-one/src/com/practice/springannotaions/SportsConfig.java
@@ -0,0 +1,10 @@
+package com.practice.springannotaions;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.practice.springannotaions")
+public class SportsConfig {
+
+}
--- spring-demo-one/src/com/practice/springannotaions/TennisCoach.java
+++ spring-demo-one/src/com/practice/springannotaions/TennisCoach.java
@@ -5,45 +5,48 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-//@Component("thatSillyCoach")
-
-//Default bean name tennisCoach with out parameter in @Component
 @Component
-@Scope()
 public class TennisCoach implements Coach {
 
-	//Field Injection
 	@Autowired
-	@Qualifier("happyFortuneService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
+	// define a default constructor
 	public TennisCoach() {
-		System.out.println(">> TennisCoach : inside default constructor");
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
 	}
 	
-	//Constructor Injection
-	/*@Autowired
-	public TennisCoach(@Qualifier("randomFortuneService") FortuneService theFortuneService) {
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");		
+	}
+	
+	// define a setter method
+	/*
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method");
+		this.fortuneService = theFortuneService;
+	}
+	*/
+	
+	/*
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
-	}*/
+	}
+	*/
 	
-	//Setter Injection
-	/*@Autowired
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach : setFortuneService() method");
-		this.fortuneService = fortuneService;
-	}*/
-	
-	//Method Injection
-	/*@Autowired
-	public void doSomeCrazyStuff(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach : doSomeCrazyStuff() method");
-		this.fortuneService = fortuneService;
-	}*/
-
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
@@ -53,16 +56,5 @@ public class TennisCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
-	
-	// define init method
-	@PostConstruct
-	public void doSomeCrazyStuff() {
-		System.out.println(">> TennisCoach : inside of doSomeCrazyStuff()");
-	}
-	
-	// define destroy method
-	@PreDestroy
-	public void doCleanupStuff() {
-		System.out.println(">> TennisCoach : inside of doCleanupStuff()");
-	}
+
 }
